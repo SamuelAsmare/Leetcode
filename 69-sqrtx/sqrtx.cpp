624ab64b46bcd8d250fab3f231918caf4c131982
Samuel Asmare <samuelasmare19@gmail.com>
2025-01-10 14:52:35 +0300

    Time: 192 ms (5.08%) | Memory: 283.4 MB (10.78%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return sqrt(x);
+        vector<int> sam; if(x<0){ return-1;}
+    for(unsigned i=0;i*i<=x;i++){
+      if(i*i>INT_MAX){return -1;}
+      sam.push_back(i);  }
+      return sam[sam.size()-1];  
         
     }
 };
