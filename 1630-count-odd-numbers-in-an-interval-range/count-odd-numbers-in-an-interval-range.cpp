113d1d4027a7850cb5663ffe56841a2c101c7027
Samuel Asmare <samuelasmare19@gmail.com>
2025-01-24 17:04:17 +0300

    Time: 1761 ms (12.21%) | Memory: 7.8 MB (35.70%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
    int countOdds(int low, int high) {
-    int counter=0;  
-    for(int i=low;i<=high;i++){
-        if(i%2!=0){counter++;}
-    } return counter; }
+    int counter=0; int i=low;
+    while(i<=high){
+        if(i%2!=0){
+            counter++;
+            
+        }i++;
+    }return counter;  }
+    
 };
