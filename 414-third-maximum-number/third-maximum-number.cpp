aecfa0e425114cba5cff4ae14518d032a8fd6320
Samuel Asmare <samuelasmare19@gmail.com>
2025-01-26 15:45:20 +0300

    Time: 0 ms (100.00%) | Memory: 12.9 MB (59.03%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        vector<int> sam;
-        for(int i=0; i<nums.size(); i++){
-        bool found=true;for(int j=1; j<=sam.size(); j++){
-
-        if(nums[i]==sam[j-1]){     found=false;    }   }
-        if(found){ sam.push_back(nums[i]);      }   }
-    for(int i=0;i<sam.size();i++){
-   for(int j=0;j<sam.size();j++){
-       if(sam[i]>sam[j]){int temp=sam[i];
-            sam[i]=sam[j];   sam[j]=temp;  } } }
-     if(sam.size()<=2){
-         return sam[0];      }
-      else{return sam[2];} 
+    sort(nums.begin(), nums.end());
+     auto sam=unique(nums.begin(), nums.end());
+     nums.erase(sam, nums.end());
+     if(nums.size()<=2){return nums[nums.size()-1];}
+     return nums[nums.size()-3];
     }
 };
