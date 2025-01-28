e2f253c9aef9f8254bdce54f498f4f9147aef40b
Samuel Asmare <samuelasmare19@gmail.com>
2025-01-28 11:39:27 +0300

    Time: 9 ms (6.45%) | Memory: 12.3 MB (61.17%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,3 +1,15 @@
 class Solution(object):
     def sortColors(self, nums):
-      return nums.sort()
+      
+      track = True
+      i=0
+      while track:
+        track = False
+        for i in range(1, len(nums)):
+            
+            if nums[i]<nums[i-1]:
+                nums[i],nums[i-1] = nums[i-1],nums[i]
+                track = True
+    
+    
+
