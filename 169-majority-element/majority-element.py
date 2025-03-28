0c91d98f8566a358e210e03182e83fa64bf5b433
Samuel Asmare <samuelasmare19@gmail.com>
2025-03-28 10:18:53 +0300

    Time: 2 ms (84.35%) | Memory: 19.8 MB (5.26%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,11 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-     max=0 
-     newindex = 0
-     fre=Counter(nums)
-     for i in range(len(nums)):
-        if(fre[nums[i]]>max):
-           max=fre[nums[i]]
-           newindex=i
-     return nums[newindex] 
-        
+        sam=Counter(nums)
+        sor=sorted(sam.items(), key=lambda x:x[1], reverse=True)
+        return (sor[0][0])
