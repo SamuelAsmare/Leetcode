19cf08212150690f2e9786fc1a2ac2250d0ff21f
Samuel Asmare <samuelasmare19@gmail.com>
2025-04-15 17:35:46 +0300

    Time: 113 ms (26.93%) | Memory: 18.7 MB (36.47%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.py
@@ -2,8 +2,7 @@ class Solution:
     def minOperations(self, nums: List[int]) -> int:
         n,steps=len(nums),0
         for i in range(1,n):
-            prev,curr=nums[i-1],nums[i]
-            if(curr<=prev):
-                steps+=prev-curr+1
-                nums[i]=curr+prev-curr+1
+            if(nums[i]<=nums[i-1]):
+                steps+=nums[i-1]-nums[i]+1
+                nums[i]=nums[i]+nums[i-1]-nums[i]+1
         return steps
