f048488be7c22ca819c1926b413aa6f87a55dd7f
Samuel Asmare <samuelasmare19@gmail.com>
2025-05-03 16:50:40 +0300

    Time: 67 ms (52.46%) | Memory: 32.9 MB (7.02%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -2,10 +2,10 @@ class Solution:
     def maxSubArray(self,nums: List[int]) -> int:
         maxval=summ=nums[0]
         for  i in range(1,len(nums)):
-            curr=nums[i]
-            if (summ + curr > curr):
+            curr=nums[i]   
+            if (summ + curr > curr): # if the sum becomes greater than the current value, i am gonna increase it
                 summ += curr
-            else:
+            else:  # but if the sum become greater than the current value, the summ becomes the current value
                 summ=curr
             maxval=max(maxval,summ)
         return maxval
