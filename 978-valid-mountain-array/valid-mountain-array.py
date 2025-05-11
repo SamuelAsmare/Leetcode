8ad11225615af1cd36a2e2a70b12159ba0134a5a
Samuel Asmare <samuelasmare19@gmail.com>
2025-05-11 19:28:58 +0300

    Time: 173 ms (13.14%) | Memory: 19.1 MB (88.08%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        if(len(arr)==1):
+            return False
+        if arr[0] > arr[1]:
+            return False
+        if arr[-1] > arr[-2]:
+            return False
+        seen = False
+        for i in range(1,len(arr)):
+            if(arr[i] == arr[i-1]):
+                return False
+            if arr[i-1] > arr[i]:
+                seen = True
+            if seen and arr[i]>=arr[i-1]:
+                return False
+        return True
