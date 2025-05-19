05547eb15d5e4a48ef8967b00210be247625cd42
Samuel Asmare <samuelasmare19@gmail.com>
2025-05-19 18:37:21 +0300

    Time: 49 ms (53.10%) | Memory: 45.5 MB (65.86%) - LeetSync

---
+++ 1731-even-odd-tree/even-odd-tree.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        q = deque([root])
+        level = 0
+        while q:
+            prev = float('-inf') if level % 2 == 0 else float('inf')
+            for _ in range(len(q)):
+                n = q.popleft()
+                if (level % 2 == 0 and (n.val % 2 == 0 or n.val <= prev)) or \
+                   (level % 2 == 1 and (n.val % 2 == 1 or n.val >= prev)):
+                    return False
+                prev = n.val
+                if n.left:
+                    q.append(n.left)
+                if n.right:
+                    q.append(n.right)
+            level += 1
+        return True
