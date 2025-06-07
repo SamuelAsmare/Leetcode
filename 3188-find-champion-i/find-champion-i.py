6390a42a6caf996dec75a952a00556d4ba3c9cdf
Samuel Asmare <samuelasmare19@gmail.com>
2025-06-07 20:32:39 +0300

    Time: 22 ms (9.45%) | Memory: 18.4 MB (43.65%) - LeetSync

--- 3188-find-champion-i/find-champion-i.py
+++ 3188-find-champion-i/find-champion-i.py
@@ -1,13 +1,10 @@
 class Solution:
     def findChampion(self, grid: List[List[int]]) -> int:
-        n = len(grid)
-        losers = set()
-
+        losers , n = set() , len(grid)
         for i in range(n):
             for j in range(n):
                 if i != j and grid[i][j] == 1:
                     losers.add(j)
-
         for i in range(n):
             if i not in losers:
                 return i
