c3b9892b9df244b32ee8d8a2501c28bb189e1a6b
Samuel Asmare <samuelasmare19@gmail.com>
2025-06-17 18:38:36 +0300

    Time: 4 ms (14.35%) | Memory: 18.1 MB (37.13%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -2,9 +2,9 @@ class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
 
         ans , n = set() , len(nums)
-        nums.sort() # to remove the duplicate subsets e
+        # nums.sort() # to remove the duplicate subsets e
         def backtrack(path , i):
-            ans.add(tuple(path.copy()))
+            ans.add(tuple(sorted(path.copy())))
             for sam in range(i , n):
                 path.append(nums[sam])
                 backtrack(path , sam+1)
