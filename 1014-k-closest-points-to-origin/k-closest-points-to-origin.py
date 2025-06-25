5306e7e4ce4a741b8485e4f677e60babd2d6e748
Samuel Asmare <samuelasmare19@gmail.com>
2025-06-25 19:36:51 +0300

    Time: 54 ms (84.19%) | Memory: 23.2 MB (16.19%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -2,9 +2,9 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         for i in range(len(points)):
             curr=points[i]
-            distance=points[i][0]*points[i][0] + points[i][1]*points[i][1]
+            d=points[i][0]*points[i][0] + points[i][1]*points[i][1]
             
-            points[i]=[curr,distance]
+            points[i]=[curr,d]
         points=sorted(points,key=lambda x : x[1])
         ans=[]
         for i in range(k):
