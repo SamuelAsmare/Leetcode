4e8f39a71c94c04472ae02e4f094935977161a89
Samuel Asmare <samuelasmare19@gmail.com>
2025-07-16 15:59:58 +0300

    Time: 19 ms (39.90%) | Memory: 19.1 MB (67.67%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -1,12 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        ans , s = [0,0] , set(nums)
-        for i in range(1, len(nums) + 1):
-            if (i not in s):
-                ans[1] = i
-            if nums.count(i)==2:
-                ans[0] = i
-            if 0 not in ans:
-                return ans
-            
-        
+        fre ,  n = Counter(nums) , len(nums)
+        duplicate = miss = 0
+        for i in range(1,n+1):
+            if fre[i] == 2:
+                duplicate = i
+            if fre[i] == 0:
+                miss = i
+            if miss and duplicate:
+                break
+        return [duplicate , miss]
