8f1ed8360c628cca1734338dea0ba88f4cb428b8
Samuel Asmare <samuelasmare19@gmail.com>
2025-07-18 23:21:28 +0300

    Time: 28 ms (70.24%) | Memory: 18.4 MB (40.09%) - LeetSync

---
+++ 654-maximum-binary-tree/maximum-binary-tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        mx = max(nums)
+        i = nums.index(mx)
+        node = TreeNode(mx)
+        node.left = self.constructMaximumBinaryTree(nums[:i])
+        node.right = self.constructMaximumBinaryTree(nums[i+1:])
+        return node
