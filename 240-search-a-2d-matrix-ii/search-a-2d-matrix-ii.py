9b286bd40632fded3515936a28b30af03bd79e6a
Samuel Asmare <samuelasmare19@gmail.com>
2025-07-29 21:14:50 +0300

    Time: 137 ms (87.85%) | Memory: 24 MB (47.43%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -1,13 +1,13 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        found=False
+        exist=False
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if(matrix[i][j] ==target):
-                    found=True
+                    exist=True
                     break
                 elif(matrix[i][j]>target):
                     break
-            if(found):
+            if(exist):
                 break
-        return found
+        return exist
