0f9eab377753eddc7e2771729f873299de5b0d62
Samuel Asmare <samuelasmare19@gmail.com>
2025-08-06 23:55:28 +0300

    Time: 66 ms (55.22%) | Memory: 51 MB (60.07%) - LeetSync

--- 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
+++ 2620-find-consecutive-integers-from-a-data-stream/find-consecutive-integers-from-a-data-stream.py
@@ -3,10 +3,8 @@ class DataStream:
         self.value = value
         self.stream = []
         self.k = k
-        self.sum = 0
         self.matchedvalues = 0
     def consec(self, num: int) -> bool:
-        self.sum += num
         if num==self.value:
             self.matchedvalues += 1
         else:
