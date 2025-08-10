2868e247bb04845385615319d6920dd83cc4c94a
Samuel Asmare <samuelasmare19@gmail.com>
2025-08-10 20:33:03 +0300

    Time: 45 ms (42.35%) | Memory: 27.9 MB (77.64%) - LeetSync

--- 398-random-pick-index/random-pick-index.py
+++ 398-random-pick-index/random-pick-index.py
@@ -7,8 +7,3 @@ class Solution:
     def pick(self, target: int) -> int:
         return random.choice(self.group[target])
         
-
-
-# Your Solution object will be instantiated and called as such:
-# obj = Solution(nums)
-# param_1 = obj.pick(target)
