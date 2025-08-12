3213cf0946d30e3f6da63e831a5dfd98061c7791
Samuel Asmare <samuelasmare19@gmail.com>
2025-08-12 09:10:56 +0300

    Time: 24 ms (35.90%) | Memory: 18.7 MB (46.67%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.py
@@ -1,9 +1,4 @@
 class Solution:
     def maximumProduct(self, nums: List[int]) -> int:
         nums.sort()
-        size=len(nums)
-        proWithNegative=nums[0]*nums[1]*nums[size-1]
-        proWithOutNegative=nums[size-1]*nums[size-2]*nums[size-3]
-        if(proWithNegative>proWithOutNegative):
-            return proWithNegative
-        return proWithOutNegative
+        return max(nums[0]*nums[1]*nums[-1],nums[-1]*nums[-2]*nums[-3])
