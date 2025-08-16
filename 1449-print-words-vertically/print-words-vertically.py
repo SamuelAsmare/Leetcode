eb16dbd28af85ac852249b4c0561a72f7a3bfc68
Samuel Asmare <samuelasmare19@gmail.com>
2025-08-16 19:24:18 +0300

    Time: 32 ms (91.30%) | Memory: 17.9 MB (7.61%) - LeetSync

--- 1449-print-words-vertically/print-words-vertically.py
+++ 1449-print-words-vertically/print-words-vertically.py
@@ -2,13 +2,8 @@ class Solution:
     def printVertically(self, s: str) -> List[str]:
         words = s.split()
         n = max(len(word) for word in words)
-        result = [] 
-        for i in range(len(words)):
-            eachrow = words[i]
-            
-            for j in range(len(words[i]),n):
-                eachrow += " "
-            words[i] = eachrow
+        for i , word in enumerate(words):
+            words[i] = words[i].ljust(n)
         vertical =["".join(word).rstrip() for word in zip(*words)]
         return vertical
                 
