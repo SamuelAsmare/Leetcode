6a12722602cde22ab839791ee948864d2acbed3d
Samuel Asmare <samuelasmare19@gmail.com>
2025-08-18 09:23:59 +0300

    Time: 0 ms (100.00%) | Memory: 18.1 MB (23.85%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.py
+++ 1217-relative-sort-array/relative-sort-array.py
@@ -1,10 +1,14 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
-        arr1.sort()
-        res = []
-        for i in range(len(arr2)):
-            while(arr2[i] in arr1):
-                res.append(arr2[i])
-                arr1.remove(arr2[i])
-        res += arr1
-        return res
+        fre = Counter(arr1)
+        ans = []
+        non_available = []
+        for item in arr1:
+            if item not in arr2:
+                non_available.append(item)
+        for item in arr2:
+            ans.extend([item]*fre[item])
+        non_available.sort()
+        ans.extend(non_available)
+        return ans
+        
