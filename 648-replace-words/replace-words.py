41e140ac85c3c0189215afeb554b1c349d432fc4
Samuel Asmare <samuelasmare19@gmail.com>
2025-09-01 23:14:56 +0300

    Time: 314 ms (7.84%) | Memory: 25.7 MB (99.70%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -5,7 +5,7 @@ class Solution:
         for i , word in enumerate(words):
             temp , replaced = "" , False
             for j in range(len(word)):
-                temp += word[j]
+                temp = word[:j]
                 if temp in roots and not replaced:
                     words[i] = temp
                     replaced =True
