5a04a0cf6505f92e87d6f80181703f2ea57c562a
Samuel Asmare <samuelasmare19@gmail.com>
2025-09-22 21:19:36 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.89%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,10 +1,10 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        fre,n=Counter(blocks[:k]),len(blocks)
+        n=len(blocks) # checking the length of the list
+        fre=Counter(blocks[:k]) # i am getting the initial frequenct
         small=fre['W']
         for i in range(k,n):
             fre[blocks[i]]+=1
             fre[blocks[i-k]]-=1
             small=min(small,fre["W"])
         return small
-        
