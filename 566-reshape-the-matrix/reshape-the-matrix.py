87c6c0ecb643d3da9259aed7f91a2574445f7f50
Samuel Asmare <samuelasmare19@gmail.com>
2025-10-13 10:20:52 +0300

    Time: 16 ms (5.28%) | Memory: 29.9 MB (6.02%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.py
+++ 566-reshape-the-matrix/reshape-the-matrix.py
@@ -1,8 +1,9 @@
-import numpy as np
+import numpy as sam
 class Solution:
     def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
-        if len(mat)*len(mat[0]) != r*c:
+        if len(mat) * len(mat[0]) != r*c:
             return mat
-        arr = np.array(mat)
-        arr = arr.reshape(r,c)
-        return arr.tolist()
+        arr = sam.array(mat)
+        ans = arr.reshape(r,c)
+        return ans.tolist()
+        
