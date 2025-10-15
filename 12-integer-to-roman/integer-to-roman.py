c4a952263538d3da5f6bf4ea3ecefa2dbb4f9ab7
Samuel Asmare <samuelasmare19@gmail.com>
2025-10-15 19:54:22 +0300

    Time: 4 ms (63.61%) | Memory: 17.9 MB (24.16%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        dic = { 1: "I",  5: "V",    4: "IV",
+            10: "X",   9: "IX", 50: "L",   40: "XL",
+            100: "C",  90: "XC", 500: "D",  400: "CD",
+            1000: "M", 900: "CM",  }
+        result = '' 
+        array = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        for value in array:
+            while value <= num:
+                result += dic[value]
+                num-=value
+        return result
