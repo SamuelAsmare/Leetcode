05f6ac805b528cd451a924f35b6a79825a9bcde0
Samuel Asmare <samuelasmare19@gmail.com>
2025-10-21 21:23:32 +0300

    Time: 8 ms (62.81%) | Memory: 20.6 MB (54.52%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.py
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.py
@@ -1,28 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def sumEvenGrandparent(self, root: Optional[TreeNode]) -> int:
-        ans  = 0
-        def inorder(root):
-            nonlocal ans
-            if root:
-                inorder(root.left)
-                if root.val % 2 == 0:
-                    if root.left and root.left.left:
-                        ans += root.left.left.val
-                    if root.left and root.left.right:
-                        ans += root.left.right.val
-                    if root.right and root.right.left:
-                        ans += root.right.left.val
-                    if root.right and root.right.right:
-                        ans += root.right.right.val
-                inorder(root.right)
-        inorder(root)
-        return ans
-
-            
-            
+        def dfs(rooot, parent, grandparent):
+            if not rooot:
+                return 0
+            total = 0
+            if grandparent and grandparent % 2 == 0:
+                total += rooot.val
+            total += dfs(rooot.left, rooot.val, parent)
+            total += dfs(rooot.right, rooot.val, parent)
+            return total
+        return dfs(root, None, None)
