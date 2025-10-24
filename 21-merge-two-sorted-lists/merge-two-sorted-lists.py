963eac07d4f07d55488e3d2d6c4f3e337d19c3d5
Samuel Asmare <samuelasmare19@gmail.com>
2025-10-24 19:45:32 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.75%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,17 +5,34 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        res , curr1 , curr2 = ListNode( None ) , list1 , list2
-        res = ListNode(3)
-        iterator = res
-        while curr1 and curr2:
-            if curr1.val>=curr2.val:
-                iterator.next = curr2
-                curr2 = curr2.next
+        if not list1 and list2:
+            return list2
+        if list1 and not list2:
+            return list1
+        if not list1 and not list2:
+            return
+        pointer1 , pointer2 = list1 , list2
+        if list1.val > list2.val:
+            merged = pointer2
+            pointer2 = pointer2.next
+        else:
+            merged = pointer1
+            pointer1 = pointer1.next
+        ans = merged
+        while pointer1 and pointer2:
+            if pointer1.val > pointer2.val:
+                merged.next = pointer2
+                merged = merged.next
+                pointer2 = pointer2.next
             else:
-                iterator.next = curr1
-                curr1 = curr1.next
-            iterator = iterator.next
-        iterator.next = curr1 if curr1 else curr2           
-        return res.next
+                merged.next = pointer1
+                merged = merged.next
+                pointer1 = pointer1.next
+        if pointer2:
+            merged.next = pointer2
+        if pointer1:
+            merged.next = pointer1
+        return ans
+
+
 
