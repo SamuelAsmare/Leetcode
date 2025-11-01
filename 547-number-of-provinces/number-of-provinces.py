31a77e830db1ac564ce5f498c993b7bf80a84604
Samuel Asmare <samuelasmare19@gmail.com>
2025-11-01 13:07:05 +0300

    Time: 3 ms (89.27%) | Memory: 19 MB (58.93%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,22 +1,37 @@
+# the question is union find 
+class UnionFind: # quick find type
+    def __init__(self,size):
+        self.parent = [i for i in range(size)]
+        self.rank = [0 for _ in range(size)]
+    def find(self , x):
+        while self.parent[x] != x:
+            x = self.parent[x]
+        return x
+    def union(self,x,y):
+        pary = self.find(y)
+        parx = self.find(x)
+        if self.rank[parx] > self.rank[pary]:
+            self.parent[pary] = parx
+        elif self.rank[pary] > self.rank[parx]:
+            self.parent[parx] = pary
+        else: # if they are equal , lets randomly assign the parent and increase the rank of the parent
+            self.parent[parx] = pary
+            self.rank[pary] += 1
+    def connected(self , x,y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph , n , visited , provinces = defaultdict(set) , len(isConnected) , set() , 0
+        province = n = len(isConnected) 
+        # if they are connected , assign the same parent and decrease the provonce
+        uf = UnionFind(n)
         for i in range(n):
             for j in range(n):
-                curr  =  isConnected[i][j]
-                if curr==1:
-                    graph[i+1].add(j+1)
-                    graph[j+1].add(i+1)
-        def dfs(graph,char,visited):
-            if char in visited:
-                return         
-            visited.add(char)
-            for j in graph[char]:
-                dfs(graph,j,visited)
-        for each in graph:
-            if each not in visited:
-                dfs(graph,each,visited)
-                provinces += 1
-        return provinces
-        
+                if i == j:
+                    continue
+                if isConnected[i][j] and uf.find(i) != uf.find(j):
+                    uf.union(i,j)
+                    province -= 1
+        return province
+
 
