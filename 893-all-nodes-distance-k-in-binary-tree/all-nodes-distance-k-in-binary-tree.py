253e39110948bb2b791dfd335864c1f5b88ee3d6
Samuel Asmare <samuelasmare19@gmail.com>
2025-11-06 15:58:08 +0300

    Time: 48 ms (19.50%) | Memory: 18.1 MB (69.59%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.py
@@ -4,7 +4,6 @@
 #         self.val = x
 #         self.left = None
 #         self.right = None
-
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         if k == 0:
