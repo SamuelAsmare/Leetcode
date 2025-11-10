9ac6a2a7bb7f9736a6fd83ecb2be2a086da5a91f
Samuel Asmare <samuelasmare19@gmail.com>
2025-11-10 16:04:46 +0300

    Time: 4 ms (61.51%) | Memory: 17.6 MB (89.45%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -2,8 +2,8 @@ class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         # get the common prefix of all numbers , (right shift)
         shifts = 0
-        while left < right:
+        while left != right:
             left  = left >> 1
             right = right >> 1
             shifts += 1
-        return left << shifts # can also right >> shifts since 
+        return right << shifts # can also right >> shifts since 
