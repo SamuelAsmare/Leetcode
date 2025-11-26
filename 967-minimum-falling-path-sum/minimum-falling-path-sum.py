fc8f76f9108eaee7491c6aa6f0e548abb4e7207c
Samuel Asmare <samuelasmare19@gmail.com>
2025-11-26 17:06:52 +0300

    Time: 74 ms (9.34%) | Memory: 27.6 MB (5.55%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -9,7 +9,7 @@ class Solution:
                 return dp[(row,col)]
             if (row,col) in dp:
                 return dp[(row,col)]
-            dp[(row, col)] += matrix[row][col] + min(dfs(row+1 , col-1),dfs(row+1,col) , dfs(row+1,col+1))
+            dp[(row, col)] = matrix[row][col] + min(dfs(row+1 , col-1),dfs(row+1,col) , dfs(row+1,col+1))
             return dp[(row, col)]
         for col , item in enumerate(matrix[0]):
             dfs(0,col)
