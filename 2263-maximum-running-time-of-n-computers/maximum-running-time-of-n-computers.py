1520df87d0ae4260e74675ee5f5e1a9d3356d8e9
Samuel Asmare <samuelasmare19@gmail.com>
2025-12-01 21:52:04 +0300

    Time: 53 ms (89.52%) | Memory: 31.1 MB (53.81%) - LeetSync

--- 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.py
@@ -2,10 +2,10 @@ class Solution:
     def maxRunTime(self, n: int, batteries: List[int]) -> int:
             batteries.sort()
             extra = sum(batteries[:-n])
-            live = batteries[-n:]
-            for i in range(n - 1):
-                if extra < (i+1)*(live[i+1] - live[i]):
-                    return live[i] + extra//(i + 1)
-                extra -= (live[i+1] - live[i])*(i+1)
-            return live[n-1] + extra//n
+            live = len(batteries) - n
+            for i in range(live,live + n - 1):
+                if extra < (i-live+1)*(batteries[i+1] - batteries[i]):
+                    return batteries[i] + extra//(i - live + 1)
+                extra -= (batteries[i+1] - batteries[i])*(i-live+1)
+            return batteries[-1] + extra//n
             
