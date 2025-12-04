eecc0164b4943015305d22d70ff9de4168abac09
Samuel Asmare <samuelasmare19@gmail.com>
2025-12-04 10:55:06 +0300

    Time: 1285 ms (98.00%) | Memory: 77 MB (96.17%) - LeetSync

--- 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.py
+++ 3897-count-number-of-trapezoids-ii/count-number-of-trapezoids-ii.py
@@ -1,53 +1,40 @@
 class Solution:
     def countTrapezoids(self, points: List[List[int]]) -> int:
         n = len(points)
-        inf = 10**9 + 7
-        slope_to_intercept = defaultdict(list)
-        mid_to_slope = defaultdict(list)
+        slope_intercept = defaultdict(list)
+        mid_slope = defaultdict(list)
         ans = 0
-
+        #  first we have to get the slope and all itercepts
         for i in range(n):
-            x1, y1 = points[i]
-            for j in range(i + 1, n):
-                x2, y2 = points[j]
-                dx = x1 - x2
-                dy = y1 - y2
-
-                if x2 == x1:
-                    k = inf
-                    b = x1
-                else:
-                    k = (y2 - y1) / (x2 - x1)
-                    b = (y1 * dx - x1 * dy) / dx
-
-                mid = (x1 + x2) * 10000 + (y1 + y2)
-                slope_to_intercept[k].append(b)
-                mid_to_slope[mid].append(k)
-
-        for sti in slope_to_intercept.values():
-            if len(sti) == 1:
+            x1 , y1 = points[i]
+            for j in range(i + 1,n):
+                x2 , y2 = points[j]
+                dx , dy  , b , slope  = x2 - x1 , y2 - y1, x1 , float("inf")
+                if x1 != x2 :
+                    slope = (dy/dx)
+                    b =  (y1 * dx - x1 * dy) / dx
+                slope_intercept[slope].append(b)
+                mid_slope[((x1 + x2)/2 , (y1 + y2)/2)].append(slope)
+        for intercepts in slope_intercept.values():
+            if len(intercepts) == 1:
                 continue
-
-            cnt = defaultdict(int)
-            for b_val in sti:
-                cnt[b_val] += 1
-
-            total_sum = 0
-            for count in cnt.values():
-                ans += total_sum * count
-                total_sum += count
-
-        for mts in mid_to_slope.values():
-            if len(mts) == 1:
-                continue
-
-            cnt = defaultdict(int)
-            for k_val in mts:
-                cnt[k_val] += 1
-
-            total_sum = 0
-            for count in cnt.values():
-                ans -= total_sum * count
-                total_sum += count
-
+            cnt = Counter(intercepts)
+            prefix = 0
+            for val in cnt.values():
+                ans += (prefix * val)
+                prefix += val
+        # count parallelograms and subtract from the answer
+        # two lines with same midpoint and different slope make parallelogram
+        for s in mid_slope.values():
+            if len(s) == 1 : continue
+            cnt = Counter(s)
+            prefix = 0
+            for item in cnt.values():
+                ans -= (item * prefix)
+                prefix += item
         return ans
+
+            
+
+
+        
