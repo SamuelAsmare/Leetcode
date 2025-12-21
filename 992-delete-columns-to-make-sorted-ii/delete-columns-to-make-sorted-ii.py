213d7400f94380478d34d71d8fe3827e9875a7ae
HeapCoder <samuelasmare19@gmail.com>
2025-12-21 15:48:14 +0300

    Time: 11 ms (25.64%) | Memory: 17.4 MB (100.00%) - LeetSync

---
+++ 992-delete-columns-to-make-sorted-ii/delete-columns-to-make-sorted-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minDeletionSize(self, strs):
+        def is_sorted(strs):
+            return all(strs[i] <= strs[i+1] for i in range(len(strs) - 1))
+        ans = 0
+        cur = [""] * len(strs)  
+        for col in zip(*strs):
+            cur2 = cur[:]
+            for i, letter in enumerate(col):
+                cur2[i] = cur2[i] + letter
+
+            if is_sorted(cur2):
+                cur = cur2
+            else:
+                ans += 1
+        return ans
