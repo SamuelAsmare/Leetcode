759345cca300bd344abaddcc2dc2dd6ca16919e1
HeapCoder <samuelasmare19@gmail.com>
2025-12-22 08:40:36 +0300

    Time: 378 ms (33.23%) | Memory: 21.6 MB (9.92%) - LeetSync

---
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        indices , ans = defaultdict(list) , 0
+        for i , char in enumerate(s):
+            indices[char].append(i)
+        print(indices)
+        for i in range(len(words)):
+            start_index = -1
+            for item in words[i]:
+                pos = bisect_right(indices[item],start_index)
+                if pos >= len(indices[item]):
+                    break
+                else:
+                    start_index = indices[item][pos]
+            else:
+                ans += 1
+        return ans 
+
+
+
+        
