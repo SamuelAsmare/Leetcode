fb51ad2e98c74cadbabcc9083f5cf4784fbfc1ae
HeapCoder <samuelasmare19@gmail.com>
2026-02-23 12:00:59 +0300

    Time: 312 ms (7.83%) | Memory: 27.7 MB (75.80%) - LeetSync

--- 648-replace-words/replace-words.py
+++ 648-replace-words/replace-words.py
@@ -1,13 +1,11 @@
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        words = sentence.split()
-        roots = set(dictionary)
+        words_set = set(dictionary)
+        words = sentence.split(" ")
         for i , word in enumerate(words):
-            temp , replaced = "" , False
             for j in range(len(word)):
-                temp = word[:j]
-                if temp in roots and not replaced:
-                    words[i] = temp
-                    replaced =True
+                if word[:j+1] in words_set:
+                    words[i] = word[:j+1]
+                    break
         return " ".join(words)
-
+                    
