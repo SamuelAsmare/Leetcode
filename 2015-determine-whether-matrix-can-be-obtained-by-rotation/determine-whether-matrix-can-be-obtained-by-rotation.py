01a940ed5260c8d083370ef06dc2284022a9f814
HeapCoder <samuelasmare19@gmail.com>
2026-03-22 14:44:54 +0300

    Time: 0 ms (100.00%) | Memory: 19.1 MB (97.54%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -1,10 +1,11 @@
-
 class Solution:
     def findRotation(self, mat: List[List[int]], target: List[List[int]]) -> bool:
-        arr = mat
+        # transposwe
         for _ in range(4):
-            arr = [list(col) for col in zip(*arr)][::-1]
-            if arr == target:
+            for i in range(len(mat)):
+                for j in range(i , len(mat)):
+                    mat[i][j] , mat[j][i] = mat[j][i] , mat[i][j]
+            mat.reverse()
+            if mat == target:
                 return True
         return False
-
